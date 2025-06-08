0112fa5875389a0685144532be15d2697ba0cd66
wiktrek <wiktor.janiszewski10@gmail.com>
2025-06-08 15:09:11 +0200

    feat: added checking for empty squares == element - bombs

--- main.go
+++ main.go
@@ -13,27 +13,35 @@ func main() {
 func rule1(grid [][]int) {
 	for i:=0;i<len(grid);i++ {
 		for j:=0;j<len(grid);j++ {
-			if grid[i][j] != 0 {
-				around, check := check_around(grid, i,j)
-				if check == grid[i][j] {
+			element := grid[i][j];
+			if element!= 0 {
+				around, empty, bombs := check_around(grid, i,j)
+				if bombs ==  element{
 					fmt.Print("Squares around are safe", i,j)
+				} else if element - bombs == empty {
+					fmt.Print("All empty squares around are bombs")
 				}
+				
 			}
 		}
 	}
 }
-func check_around(grid [][]int,i int, j int) ([]int, int) {
+func check_around(grid [][]int,i int, j int) ([]int, int, int) {
 	around := make([]int,8)
 	touching := [][]int{{-1,-1}, {-1,0}, {-1, 1}, {0, 1}, {0,0},{0,-1},{1,1},{1,0},{1,-1}}
-	empty_squares_around:= 0
+	bombs_around:= 0
+	empty_squares_around := 0
 	for v:=0;v<len(touching);v++ {
 		element := grid[touching[v][0]][touching[v][1]];
 		if element == 9 || element == 10 {
-			empty_squares_around += 1;
+			bombs_around += 1
 		}
+		if element == 0 {
+			empty_squares_around += 1
+		} 
 		around = append(around, element)
 	}
-	return around, empty_squares_around
+	return around, empty_squares_around, bombs
 }
 func get_grid(size int) [][]int {
 	grid := make([][]int, size)
